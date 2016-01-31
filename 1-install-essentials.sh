67ccbe9a0e82664e124f65cfedf55231bc4be8b9
Percy Grunwald <percy.grunwald@gmail.com>
2016-01-31 17:34:36 +0800

    Adds node.js repo for 5.5.0+, local rebar and sets postgres password to 'postgres'

--- 1-install-essentials.sh
+++ 1-install-essentials.sh
@@ -4,8 +4,11 @@
 wget -P ~ https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && \
 sudo dpkg -i ~/erlang-solutions_1.0_all.deb
 
+# Add node.js repo and run apt-get update
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+
 # Update repos and upgrade
-sudo apt-get update
+# sudo apt-get update
 sudo apt-get upgrade -qq
 
 # Install Packages
@@ -19,5 +22,9 @@ postgresql-9.3 \
 nodejs esl-erlang elixir inotify-tools
 
 # Install Phoenix
+yes | mix local.rebar
 yes | mix local.hex
 yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+
+# Set password for postgres user
+sudo -u postgres psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
